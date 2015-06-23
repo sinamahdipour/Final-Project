f27f05f5b85551bd690d17d35447fabbd205aef8
farnaz75 <far200284@yahoo.com>
2015-06-23 16:49:36 +0430

    salm

---
+++ FinalProject/src/finalproject/ads.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package finalproject;
+
+/**
+ *
+ * @author Asus
+ */
+public class ads {
+    private int x =20;
+}
